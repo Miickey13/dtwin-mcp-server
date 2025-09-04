920c5511a4d645a969136fdc1b6775afa94debdc
Mike <miloslav_todorov@abv.bg>
2025-09-04 09:50:35 +0300

    Fix port for render.com

--- main.py
+++ main.py
@@ -244,8 +244,8 @@ def dtwin_search(args: DtwinSearchArgs) -> Dict[str, Any]:
 if __name__ == "__main__":
     print("Starting FastMCP server...")
     # Set the host to 0.0.0.0 to allow external connections for render.com
-    # mcp.settings.host = "0.0.0.0"
-    mcp.settings.host = "127.0.0.1"
+    mcp.settings.host = "0.0.0.0"
+    # mcp.settings.host = "127.0.0.1"
     mcp.run(transport="streamable-http")
 
 
